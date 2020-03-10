4bbc30da0b9d471863ae6d0687104542cfdc417d
Patrick <patrick.peycker@outlook.com>
2020-03-10 14:49:28 +0100

    Need git ignore

---
+++ PermaFungiApp/Models/ProduitExtension.cs
@@ -0,0 +1,35 @@
+﻿using PermaFungi.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PermaFungiApp.Models
+{
+    public static class ProduitExtension
+    {
+        public static ProduitTO ToTransferObject(this Produit produit)
+        {
+            return new ProduitTO
+            {
+                idProduit = produit.IdProduit,
+                nomProduit = produit.NomProduit,
+                quantite = produit.Quantite,
+                prix = produit.Prix,
+                description = produit.Description
+            };
+        }
+
+        public static Produit ToEF(this ProduitTO produit)
+        {
+            return new Produit
+            {
+                IdProduit = produit.idProduit,
+                NomProduit = produit.nomProduit,
+                Quantite = produit.quantite,
+                Prix = produit.prix,
+                Description = produit.description
+            };
+        }
+    }
+}
