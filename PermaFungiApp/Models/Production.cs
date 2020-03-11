92f6c32b31f38b11dd4c5389d618fc2532de4bb9
Patrick <patrick.peycker@outlook.com>
2020-03-11 14:39:36 +0100

    Implementation get(deb fin id)

---
+++ PermaFungiApp/Models/Production.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PermaFungiApp.Models
+{
+
+    public class Production
+    {
+        public string[] Mois = new string[12] { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre" };
+        public double[] Pleurottes = new double[12];
+        public double[] Pieds = new double[12];
+        public double[] Panicaults = new double[12];
+    }
+}
---
+++ PermaFungiApp/Models/Ventes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PermaFungiApp.Models
+{
+    public class Ventes
+    {
+        public string[] Mois = new string[12] { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre" };
+        public double[] MarcCollectes = new double[12];
+        public double[] Kit = new double[12];
+        public double[] Fungipop = new double[12];
+    }
+}
